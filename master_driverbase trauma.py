39ea656c038761248f8b88cc0601f5a1390b80a9
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-12-11 16:40:33 +0100

    wuhoo

--- master_driverbase trauma.py
+++ master_driverbase trauma.py
@@ -1,215 +1,124 @@
-#!/usr/bin/env pybricks-micropython
-import pybricks.tools as pt
-from pybricks.ev3devices import ColorSensor, Motor, TouchSensor  # type: ignore
-from pybricks.hubs import EV3Brick
-from pybricks.parameters import Color, Direction, Port
-from pybricks.robotics import DriveBase
+def load_and_print():
+    file = open("mapa.txt", "r", encoding="utf8")
 
-ev3 = EV3Brick()    
-
-m_l = Motor(Port.C, positive_direction=Direction.COUNTERCLOCKWISE, gears=[12,20])
-m_r = Motor(Port.D, positive_direction=Direction.COUNTERCLOCKWISE, gears=[12,20])
-
-cl1 = ColorSensor(Port.S1)
-cl2 = ColorSensor(Port.S2)
-cl3 = ColorSensor(Port.S3)
-
-navig = ColorSensor(Port.S4)
-
-robot = DriveBase(m_l, m_r, 57, 226)
-
-# robot.settings(256, 1024, 128, 512)
-# robot.settings(256, 1024, 128, 512)
-
-def line():
-    global targ
-    global p
-    global base_speed
-    global navi
-
-    navi = navig.reflection()
-    error = navi - targ
-    turn = p * error
-
-    robot.drive(base_speed, turn)  # type: ignore
-
-def make_Uturn():
-    # for x in range (3):
-    robot.turn(182)
-    # robot.straight(20)
-
-def make_right():
-    ## vylaď
-    robot.straight(20)
-    robot.turn(92)
-
-def make_left():
-    ## vylaď
-    robot.straight(20)
-    robot.turn(-92)
-
-def make_strght():
-    # for skipping crossing
-    # robot.straight(30)
-    pass
-
-def rd_fwd():
-    global lft_fwd
-    global rgh_fwd
-    global mid_fwd
-    ## vylaď
-    robot.straight(30)
-
-    lft_fwd = cl1.reflection()
-    mid_fwd = cl2.reflection()
-    rgh_fwd = cl3.reflection()
-
-def bila(inp):
-    global thresh_up
-    global thresh_dwn
-
-    if inp > thresh_up: return True
-    elif inp < thresh_dwn: return False
-    else: return None
-
-def rd_all():
-    global lft
-    global rgh
-    global mid
-
-    lft = cl1.reflection()
-    mid = cl2.reflection()
-    rgh = cl3.reflection()
-
-def pebug():
-    global lft, rgh, mid
-    global navi    
-    global lft_fwd, rgh_fwd, mid_fwd
+    file = file.readlines()
+    blud = []
+    for x in file:
+        x = x[:-1]
+        temp = []
+        for y in x:
+            temp.append(y)
+        blud.append(temp)
+    # for x in blud:
+    #     print(x)
+    return blud
+def posun_na_dalsi_kriz():
     global pozice, wturn
+    global blud
 
-    print(bila(lft_fwd), bila(mid_fwd), bila(rgh_fwd), lft_fwd, mid_fwd, rgh_fwd)
-    print(bila(lft), bila(mid), bila(rgh), lft, mid, rgh)
-    print(navi)
-    print(memory)
-    print(pozice, "pozice", wturn, "wturn")
-    print()
+
+    dx, dy = 0, 0
+    if wturn == 0:
+        if not blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] in "┘ └ ├ ┤ ┴ ┼ ▫":
+            raise Exception("nemůžu nahoru, chyba")
+        dx = -1
+
+    elif wturn == 1:
+        if not blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] in "└ ┌ ├ ┬ ┴ ┼ ▫":
+            raise Exception("nemůžu doprava, chyba")
+        dy = 1
+
+    elif wturn == 2:
+        if not blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] in "┐ ┌ ┤ ├ ┬ ┼ ▫":
+            raise Exception("nemůžu dolů, chyba")
+        dx = 1
+
+    elif wturn == 3:
+        if not blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] in "┘ ┐ ┤ ┬ ┴ ┼ ▫":
+            raise Exception("nemůžu doleva, chyba")
+        dy = -1
+
+    while True:
+        pozice[0] += dx
+        pozice[1] += dy
+
+        # print(pozice)
+
+        if wturn == 0 or wturn == 2:
+            if blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] != "│":
+                break
+        if wturn == 1 or wturn == 3:
+            if blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]] != "─":
+                break
+
+blud = load_and_print()
 
 
 def make_decision(kriz):
     global pozice, wturn
-    global memory
+    global planek
+    global debug
 
-    moznosti = {"U": ["U"], "L":["L"], "R":["R"], "J":["S", "L"], "K":["S","R"], "T":["R","L"], "+":["R","L","S"]}
-    # kompletní update políčka
-    if kriz == "R":
-        memory[pozice[0]][pozice[1]].
+    if kriz == "▫":
+        # make_Uturn()
+        # updt_kola()
+        wturn = (wturn + 2) % 4 # opačný směr
+    elif kriz == "■":
+        print("finish")
+        exit()
+    else:
+        # když jsem tam nebyl, tak si vložit políčko do planek
+        policsto = planek[pozice[0]][pozice[1]]
+        if policsto.je_prazdny():
+            policsto.nastav_podle_krizovatky(kriz)
+
+        # zapsat směr příjezdu (odkud jsem přijel)
+        policsto.prijezd(wturn)
+
+        # vyhodnotím směr odjezdu
+            # zjistím kam
+        for x in planek:
+            for y in x:
+                print(y.print_all(), end="")
+            print()
+            # updt wturn
+        print()
+
+        if False:
+            while True:
+                inp = input()
+                if str(inp) in "0123":
+                    break
+            wturn = int(inp)
+        else:
+            x = policsto.get_smer()
+            debug.append(x)
+            # print(debug)
+            wturn = x
+        
+        # zapsat směr odjezdu
+            # updt_kola()
+        policsto.odjezd(wturn)
+
+        # další křižovatka (neboli vyskočím do checku a pokračuji)
 
 
 
 
-    
 
 
 
 
 def check():
-    global lft
-    global rgh
-    global mid
+    global blud
+
+    # !!!!!!!!!!!PŘIDAT ODROTOVÁNÍ
+
+    krizovatka = blud[pozice[0] - rozdil[0]][pozice[1] - rozdil[1]]
+
+    make_decision(krizovatka)
     
-    global lft_fwd
-    global rgh_fwd
-    global mid_fwd
-    global navi
-
-    global somenum
-    rd_all()
-
-    mid = cl2.reflection()
-
-    somenum += 1
-    krizovatka = None
-
-    if somenum % 100 == 0:
-        pebug()
-
-    if bila(lft) == False and bila(mid) == False and bila(rgh) == True:
-        rd_fwd()
-        somenum = 0
-        if bila(lft_fwd) == True and bila(mid_fwd) == True and bila(rgh_fwd) == True:
-            krizovatka = "L"
-        elif bila(lft_fwd) == True and bila(mid_fwd) == False and bila(rgh_fwd) == True:
-            krizovatka = "J"
-        else:
-            print("FUCK FFT!!!")
-
-          
-
-    elif bila(lft) == True and bila(mid) == False and bila(rgh) == False:
-        rd_fwd()
-        somenum = 0
-        if bila(lft_fwd) == True and bila(mid_fwd) == True and bila(rgh_fwd) == True:
-            krizovatka = "R"
-        elif bila(lft_fwd) == True and bila(mid_fwd) == False and bila(rgh_fwd) == True:
-            krizovatka = "K"
-        else:
-            print("FUCK TFF!!!")
-
-    elif bila(lft) == False and bila(mid) == False and bila(rgh) == False:
-        rd_fwd()
-        somenum = 0
-        if bila(lft_fwd) == True and bila(mid_fwd) == True and bila(rgh_fwd) == True:
-            krizovatka = "T"
-        elif bila(lft_fwd) == True and bila(mid_fwd) == False and bila(rgh_fwd) == True:
-            krizovatka = "+"
-        elif bila(lft_fwd) == False and bila(mid_fwd) == False and bila(rgh_fwd) == False:
-            print("FINISH")
-            return "out"
-
-    elif bila(lft) == True and bila(mid) == True and bila(rgh) == True:
-        rd_fwd()
-        if bila(lft_fwd) == True and bila(mid_fwd) == True and bila(rgh_fwd) == True:
-            krizovatka = "U"
-        else:
-            print("FUCK TTT!!!")
-            # make_strght()
-    elif bila(lft) == True and bila(mid) == False and bila(rgh) == True:
-        rd_all()
-        updt_kola()
-        # sledování čáry
-        pass
-    else:
-        print("NĚCO JE TOTÁLNĚ ŠPATNĚ")
-
-
-    if krizovatka != None:
-        make_decision(krizovatka)
-    
-
-
-def updt_kola():
-    global pozice, wturn
-
-    posun = round(robot.distance() / 150, 0)
-    otoceni = (round(robot.angle() / 90, 0) + wturn) % 4
-    wturn = otoceni
-
-    if otoceni == 0:
-        pozice[1] -= posun  # type: ignore
-    elif otoceni == 1:
-        pozice[0] += posun  # type: ignore
-    elif otoceni == 2:
-        pozice[1] += posun  # type: ignore
-    elif otoceni == 3:
-        pozice[0] -= posun  # type: ignore
-    else:
-        print("MOTHERFUCKER!!!!")
-
-
-    print(posun, "posun")
-    print(otoceni, "otoceni")
-
-    robot.reset()
+    posun_na_dalsi_kriz()
 
 class policko():
     # None - nevíme, -1 - cesta není, 0 cesta je, neprošli, 1 - c je, jednou p, 2 - cesta je, prošli 2 krát
@@ -219,6 +128,59 @@ class policko():
         self.doleva = None
         self.dolu = None
 
+    def je_prazdny(self):
+        if self.nahoru == None and self.doprava == None and self.dolu == None and self.doleva == None:
+            return True
+        else:
+            return False
+    
+    def nastav_podle_krizovatky(self, krizov):
+        global moznosti_krizovatky
+
+        moznosti = moznosti_krizovatky.get(krizov) # type: ignore
+        print(moznosti, krizov)
+
+        self.nahoru = moznosti[0]     # type: ignore
+        self.doprava = moznosti[1]    # type: ignore
+        self.dolu = moznosti[2]     # type: ignore
+        self.doleva = moznosti[3]       # type: ignore
+
+        strung = str([self.nahoru, self.doprava, self.dolu, self.doleva]) # type: ignore
+        print(strung)
+
+    def prijezd(self, smer):
+        if smer == 0: self.dolu += 1          # type: ignore
+        if smer == 1: self.doleva += 1        # type: ignore  
+        if smer == 2: self.nahoru += 1        # type: ignore
+        if smer == 3: self.doprava += 1       # type: ignore
+## odřřádkování
+    def odjezd(self, smer):
+        if smer == 0: self.nahoru += 1      # type: ignore
+        if smer == 1: self.doprava += 1     # type: ignore  
+        if smer == 2: self.dolu += 1        # type: ignore
+        if smer == 3: self.doleva += 1      # type: ignore
+
+    def print_all(self):
+        strung = str([self.nahoru, self.doprava, self.dolu, self.doleva]) # type: ignore
+        strung = strung.replace("None", ".")
+        strung = strung.replace("[", "")
+        strung = strung.replace("]", "")
+        strung = strung.replace("-1", "X")
+        strung = strung.replace(",", "")
+        strung = strung.replace(" ", "")
+        strung += " "
+        return strung
+
+    def get_smer(self):
+        lst = [self.nahoru, self.doprava, self.dolu, self.doleva]
+        # print(lst)
+        for x in range(len(lst)):
+            if lst[x] == -1:
+                lst[x] = 3
+        kam = lst.index(min(lst)) # type: ignore
+        return kam
+        
+        
     def gnahoru(self):
         return self.nahoru
     def gdoprava(self):
@@ -228,12 +190,6 @@ class policko():
     def gdolu(self):
         return self.dolu
     
-    def update_by_list(self, lst, posun):
-        for x in range(4):
-            y = (x + posun) % 4
-            if y == 0:
-                # self.nnahoru()
-
 
     def nnahoru(self, x):
         self.nahoru = x
@@ -246,74 +202,40 @@ class policko():
 
 
 
-pozice = [25, 25]
-wturn = 0
+pozice = [4,3]
+start = [4,3]
+rozdil = [pozice[0] - start[0], pozice[1] - start[1]]
+print(rozdil)
+debug = []
+wturn = 1
 
+moznosti_krizovatky = {
+    "┌" : [-1, 0, 0,-1],
+    "┐" : [-1,-1, 0, 0],
+    "┘" : [ 0,-1,-1, 0],
+    "└" : [ 0, 0,-1,-1],
+    "├" : [ 0, 0, 0,-1],
+    "┤" : [ 0,-1, 0, 0],
+    "┬" : [-1, 0, 0, 0],
+    "┴" : [ 0, 0,-1, 0],
+    "┼" : [ 0, 0, 0, 0],
+    "▫" : [ 0, 0, 0, 0]
+}
 
 planek = []
-for _ in range(50):
+for _ in range(16):
     temp = []
-    for _ in range(50):
-        tmp = []
-        for _ in range(4):
-            tmp.append(policko())
-        temp.append(tmp)
+    for _ in range(16):
+        temp.append(policko())
     planek.append(temp)
+# for x in planek:
+    # print(x)
 
-
-cis = 0
-najeto_na_kolech = []
-najeto_na_kolech2 = []
-
-p = 2.5
-base_speed = 75
-
-thresh_up = 26
-thresh_dwn =  16
-targ = 12
-
-memory = []
-somenum = 0
-navi = navig.reflection()
-
-# precise position
-ev3.light.off()
-while navi != targ:
-    navi = navig.reflection()
-ev3.light.on(Color.GREEN)
-pt.wait(500)
-
-
-
-rd_all()
-rd_fwd()
-
+# memory = []
+import time
 while True:
-    rd_all()
-    line()
+    time.sleep(0.)
+    ## posun()
     if check() == "out":
         break
-
-
-
-
-strung = ""
-
-for x in memory:
-    strung += x
-
-print(strung)
-
-strung_start = None
-
-
-while strung_start != strung:
-    strung_start = strung
-    strung = strung.replace("LUL", "S")
-    strung = strung.replace("LUS", "R")
-    strung = strung.replace("SUL", "R")
-    strung = strung.replace("RUL", "U")
-    strung = strung.replace("SUS", "U")
-    strung = strung.replace("UU", "")
-
-print(strung)
+    # break
